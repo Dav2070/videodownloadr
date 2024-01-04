adc102941452dfaf06b6b0296c950fb6edbf8394
David <zurek.dav@gmail.com>
2024-01-04 15:35:43 +0100

    Install & setup dav-ui-components and remove default layout of AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterOutlet } from '@angular/router'
+import { Apollo, gql } from 'apollo-angular'
+import * as DavUIComponents from 'dav-ui-components'
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
+	imports: [CommonModule, RouterOutlet],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'videodownloadr';
+	constructor(private apollo: Apollo) {
+		DavUIComponents.setLocale('en-US')
+	}
+
+	async ngOnInit() {
+		let result = await this.apollo
+			.query({
+				query: gql`
+					query DownloadYoutubeVideo($url: String!) {
+						downloadYoutubeVideo(url: $url) {
+							url
+						}
+					}
+				`,
+				variables: {
+					url: 'https://www.youtube.com/watch?v=s9mrozk-bGQ&list=PLLv3qeuV3YDpR4x4iEJZmcZWyDKGXB0Yl&index=9&ab_channel=ThrillerSoundtrackMusic'
+				}
+			})
+			.toPromise()
+
+		console.log(result)
+	}
 }
