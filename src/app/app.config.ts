72ba781647d737a13bb73b71e104fb134fec1732
David <zurek.dav@gmail.com>
2024-03-19 23:21:46 +0100

    Setup SSR

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from '@angular/core'
+import { provideRouter } from '@angular/router'
 
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { graphqlProvider } from './graphql.provider';
+import { routes } from './app.routes'
+import { provideHttpClient } from '@angular/common/http'
+import { graphqlProvider } from './graphql.provider'
+import { provideClientHydration } from '@angular/platform-browser'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), graphqlProvider]
-};
+	providers: [
+		provideRouter(routes),
+		provideHttpClient(),
+		graphqlProvider,
+		provideClientHydration()
+	]
+}
